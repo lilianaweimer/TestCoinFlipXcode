853e62f3285da6ba809085503356bf11e141485b
Liliana Weimer <58522345+lilianaweimer@users.noreply.github.com>
2024-04-17 16:02:54 -0600

    Initial Commit

---
+++ TestApp Watch App/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp Watch App
+//
+//  Created by Liliana Weimer on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct TestApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
